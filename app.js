25836e44df4c4b4c1caaaf48e260832964afd9e6
Gabegit config --global user.email escobedo.gabe@my.codergv.org <gabriel.escobedo01@utrgv.edu>
2016-11-01 21:31:01 -0500

    friends

---
+++ app.js
@@ -0,0 +1,16 @@
+var friends = {
+    bill: {
+        firstName: "Bill",
+        lastName: "Gates",
+        number: "(206) 555-5555",
+        address: ['One Microsoft Way', 'Redmond', 'WA', '98502']
+    },
+    steve: {
+        firstName: "Steve",
+        lastName: "Jobs",
+        number: "(256) 555-6666",
+        address: ['1 Infinite Loop', 'Cupertino', 'CA', '95014']
+    }
+};
+
+console.log(friends.bill.address[0]);  //One Microsoft Way
